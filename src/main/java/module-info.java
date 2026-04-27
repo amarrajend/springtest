9b5fe18b45c4da15a33ebd3f1cf32bb80b888754
amarrajend <amarrajend@gmail.com>
2026-04-27 07:50:28 +0530

    exposing the application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,17 @@
 module com.ourkadai.sp.infra {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
     requires spring.context;
     requires spring.beans;
     requires spring.web;
+    requires spring.core;
 
+    requires java.base;
+    requires spring.jdbc;
+    requires java.sql;
+
+    opens com.ourkadai.sp to spring.core, spring.beans, spring.context;
+    opens com.ourkadai.sp.db to spring.core, spring.beans, spring.context;
+
+    exports com.ourkadai.sp;
 }
